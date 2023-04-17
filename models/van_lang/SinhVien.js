10118cdaca436e489811f24f79bad7cca3ac5ee4
dnguyen <eonho6582@gmail.com>
2023-04-18 02:02:38 +0700

    add authentication

--- models/van_lang/SinhVien.js
+++ models/van_lang/SinhVien.js
@@ -5,9 +5,10 @@ const SinhVienSchema = new mongoose.Schema(
     tenSV: { type: String, require: true },
     lopSV: { type: String, require: true },
     SDT: { type: Number, require: true },
-    id_khoa: { type: mongoose.Types.ObjectId, require: true, ref: "Khoa" },
-    id_MH: [{ type: mongoose.Types.ObjectId, require: true, ref: "MonHoc" }],
-    MSSV: { type: String, require: true },
+    khoa: { type: mongoose.Types.ObjectId, require: true, ref: "Khoa" },
+    MH: [{ type: mongoose.Types.ObjectId, default: [], ref: "MonHoc" }],
+    MSSV: { type: String, require: true, unique: true },
+    khoaHoc: { type: String, require: true },
     userName: { type: String, require: true },
     // nhớ hash passowrd
     pass: { type: String, require: true },
--- routes/Student.js
+++ routes/Student.js
@@ -1,45 +1,75 @@
 const router = require("express").Router();
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 const Khoa = require("../models/van_lang/Khoa");
-const GiangVien = require("../models/van_lang/GiangVien");
-const SinhVien = require("../models/van_lang/SinhVien");
 const MonHoc = require("../models/van_lang/MonHoc");
+const SinhVien = require("../models/van_lang/SinhVien");
+const { verifyToken } = require("./VerifyToken");
 
-router.post("/students", async (req, res) => {
-  let students;
-  let count;
+router.get("/:id", verifyToken, async (req, res) => {
+  console.log(req.params);
   try {
     // students = await Khoa.find();
     // count = await Khoa.find().count();
-    let a = await SinhVien.find({ _id: req.body.id })
-      .populate(["id_MH", "id_khoa"])
+    let sinhVienData = await SinhVien.find({ _id: req.params.id })
+      .populate(["MH", "khoa"])
       .exec();
-
-    res.status(200).json({ data: a, count: 10 });
+    res.status(200).json(sinhVienData);
   } catch (error) {
     console.log(error);
-    res.status(500);
+    res.status(500).json(error);
   }
 });
 
-// router.post("/create", async (req, res) => {
-//   let students;
-//   let count = 20;
-//   try {
-//     console.log(req.body);
-//     students = await Khoa.find();
-//     const student = new GiangVien({
-//       tenGV: req.body.tenGV,
-//       SDT: req.body.SDT,
-//       MSGV: req.body.MSGV,
-//       id_khoa: students[0]._id,
-//     });
-//     let a = await student.save();
-//     console.log(a);
-//     res.status(200).json({ items: a, count });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500);
-//   }
-// });
+router.post("/register", async (req, res) => {
+  const student = new SinhVien({
+    tenSV: req.body.tenSV,
+    lopSV: req.body.lopSV,
+    SDT: req.body.SDT,
+    khoa: req.body.khoa,
+    MSSV: req.body.MSSV,
+    khoaHoc: req.body.khoaHoc,
+    userName: req.body.userName,
+    // nhớ hash passowrd
+    pass: CryptoJS.AES.encrypt(req.body.pass, process.env.PASS_SEC).toString(),
+  });
+  try {
+    let saveStudent = await student.save();
+    res.status(200).json(saveStudent);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const student = await SinhVien.findOne({ userName: req.body.userName });
+    if (!student) {
+      res.status(401).json("Wrong credentials!");
+    }
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      student.pass,
+      process.env.PASS_SEC,
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    if (OriginalPassword !== req.body.pass) {
+      res.status(401).json("Wrong credentials!");
+    }
+    const accessToken = jwt.sign(
+      {
+        id: student.userName,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "1d" },
+    );
+
+    const { pass, ...others } = student._doc;
+    res.status(200).json({ ...others, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
